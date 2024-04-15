8d34ef1bd68ce80e3e02b1f58d9698e43be59db0
Larysa Rud <larpavrud@gmail.com>
2024-04-15 19:32:56 -0400

    Feeling loopy using For of loop

--- script.js
+++ script.js
@@ -66,3 +66,41 @@ outerLoop: while (!foundPrime) {
 }
 
 console.log(nextPrime)
+
+//Part 3: Feeling Loopy
+// Using For of loop
+
+const str = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+let column = 1;
+
+for (const c of str) {
+	if (c === '\n') {
+        console.log(cell1, cell2, cell3, cell4);
+        cell1 = "";
+        cell2 = "";
+        cell3 = "";
+        cell4 = "";
+        column = 1;
+    } else if (c === ',') {
+        column++;
+    } else {
+        if (column == 1) {
+            cell1 += c;
+        } else if (column == 2) {
+            cell2 += c;
+        } else if (column == 3) {
+            cell3 += c;
+        } else if (column == 4) {
+            cell4 += c;
+        }
+    }
+  }
+
+  if (cell1.length > 0) {
+    console.log(cell1, cell2, cell3, cell4);    
+  }
