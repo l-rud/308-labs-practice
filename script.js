5b0a97b210717b72086cfd95456aa19cbaa357e6
Larysa Rud <larpavrud@gmail.com>
2024-04-15 19:36:53 -0400

    Feeling Loopy using  For loop and switch

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ console.log(nextPrime)
 //Part 3: Feeling Loopy
 // Using For of loop
 
+/*
 const str = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
 let cell1 = "";
@@ -104,3 +105,56 @@ for (const c of str) {
   if (cell1.length > 0) {
     console.log(cell1, cell2, cell3, cell4);    
   }
+  */
+
+
+  //Part 3: Feeling Loopy
+  //Using  For loop and switch
+
+let csvStr = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26"
+// let csvStr = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+
+
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+let cell = 1
+// let row = 1
+
+for (let i = 0; i < csvStr.length; i++) {
+
+  if (csvStr[i] === ','){
+    cell++
+    continue
+  }
+
+  if (csvStr[i] === "\n") {
+    cell = 1
+    cell1 = "";
+    cell2 = "";
+    cell3 = "";
+    cell4 = "";
+    continue
+  }
+
+  switch (cell) {
+    case 1:
+      cell1 += csvStr[i]
+    break
+    case 2:
+      cell2 += csvStr[i]
+    break
+    case 3:
+      cell3 += csvStr[i]
+    break
+    case 4:
+      cell4 += csvStr[i]
+    break
+  }
+
+  if (cell === 4 && csvStr[i + 1] === '\n' || i + 1 === csvStr.length) {
+    console.log(cell1, cell2, cell3, cell4);
+  }
+
+}
