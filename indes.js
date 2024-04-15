c814886654f4e653720cb2ed7e7d16a862c18171
Larysa Rud <larpavrud@gmail.com>
2024-04-15 19:21:54 -0400

    Finding prime numbers using For loop

--- indes.js
+++ indes.js
@@ -11,4 +11,31 @@ for (let i = 1; i <= 100; i++){
     }
   }
   
+  // Prime numbers
   
+  /*/*Declare an arbitrary number, n.
+Create a loop that searches for the next prime number, 
+starting at n and incrementing from there.
+As soon as you find the prime number, 
+log that number and exit the loop. */
+
+  //For loop:
+
+let n = 89;
+let i;
+let prime;
+for (i=n+1; true; i++)
+{
+    prime = true;
+    for (j = 2; j < i; j++){
+        if (i % j == 0){
+            prime = false;
+            break;
+        }
+    }
+
+    if (prime) {
+        console.log(`Prime number is  ${i}`);
+        break;
+    }
+}
